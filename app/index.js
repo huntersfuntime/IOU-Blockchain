cfd204876e271f8a27e2fd0db23a91e5ddd0e391
huntersfuntime <hunterorges@gmail.com>
2018-04-03 20:49:53 -0600

    Tested generated API using postman, created script for starting server

--- app/index.js
+++ app/index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const Blockchain = require('../blockchain');
 
-const HTTP_PORT = proccess.env.HTTP_PORT || 3001;
+const HTTP_PORT = process.env.HTTP_PORT || 3001;
 
 const app = express();
 const bc = new Blockchain();
 
-app.get('/blocks', (req, res) =>{
+app.get('/blocks', (req, res) => {
     res.json(bc.chain);
 });
 
