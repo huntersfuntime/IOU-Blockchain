f791785f1534cb4add6edf71ccc5d790b1931fbc
huntersfuntime <hunterorges@gmail.com>
2018-04-05 20:01:24 -0600

    Created multiple sockets for blockchain

--- app/p2p-server.js
+++ app/p2p-server.js
@@ -14,7 +14,18 @@ class P2pServer {
     listen() {
         const server = new Websocket.Server({ port: P2P_PORT });
         server.on('connection', socket => this.connectSocket(socket));
+
+        this.connectToPeers();
+
         console.log(`Listening for peer-to-peer connections on: ${P2P_PORT}`);
+    }
+
+    connectToPeers() {
+        peers.forEach(peer => {
+            const socket = new Websocket(peer);
+
+            socket.on('open', () => this.connectSocket(socket));
+        });
     }
 
     connectSocket(socket) {
@@ -22,3 +33,5 @@ class P2pServer {
         console.log('Socket connected');
     }
 }
+
+module.exports = P2pServer;
